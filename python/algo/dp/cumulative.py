2e2b8840bc06b58d42bf66719b2c34dfdf51c6de
jas <fourdusk4@gmail.com>
2025-10-16 23:49:15 +0800

    chore: fix answer for house robber with negative

--- python/algo/dp/cumulative.py
+++ python/algo/dp/cumulative.py
@@ -3,17 +3,22 @@
 # LC 198 - House Robber
 # but with negative numbers
 def no_adjacent_nums(nums: list[int]) -> int:
-    take = skip = 0
-    for num in nums:
+    take = nums[0]
+    skip = float("-inf")
+    best = nums[0]
+    for num in nums[1:]:
         new_take = skip + num
         new_skip = max(take, skip)
         take, skip = new_take, new_skip
-    return max(take, skip)
+        best = max(best, take, skip, num)
+        
+    return int(best)
 
 def test_simple():
     tcs = [
         ([1,-1,-1,1], 2),
-        ([1,2,3,1], 4)
+        ([1,2,3,1], 4),
+        ([-2,-1,-1,-3], -1),
     ]
 
     for nums, expected in tcs:
