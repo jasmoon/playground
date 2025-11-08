422af4192f37e47993592193197a735accac4fb7
jas <fourdusk4@gmail.com>
2025-11-08 20:02:54 +0800

    feat: add and complete part 2 of event tracker

--- python/system_algo/event_tracker.py
+++ python/system_algo/event_tracker.py
@@ -4,13 +4,13 @@
 # and provides approximate analytics.
 
 # APIs
-# Implement the following methods:
+# Part 1, implement the following methods:
 # def record_event(self, user_id: str, event_type: str, timestamp: int) -> None:
 #     """
 #     Record that a user triggered an event (e.g. 'view', 'like') at the given timestamp (seconds).
 #     """
 
-# def update_event(self, user_id: str, old_timestamp: int, new_timestamp: int) -> bool:
+# def update_event_timestamp(self, user_id: str, old_timestamp: int, new_timestamp: int) -> bool:
 #     """
 #     Update the timestamp of a previously recorded event.
 #     Return False if the old event doesn’t exist or is too old to update.
@@ -41,6 +41,25 @@
 # - Analytics must remain monotonic within each bucket
 # - When an event is deleted or moved, the aggregate for that bucket must decrease accordingly.
 # - Concurrency awareness (for extra credit): Discuss or design where locks would be placed if this were a multi-threaded tracker.
+
+# Part 2, implement the following APIs:
+
+
+# def delete_user(self, user_id: str) -> bool:
+#     """
+#     Delete an existing user and all user related data.
+#     Return False if the user doesn’t exist.
+#     """
+
+# def get_top_active_users(self, k: int):
+#     """
+#     Return the top-k users by frequency of events
+#     """
+
+# def get_top_active_users_by_event_type(self, event_type: str, k: int):
+#     """
+#     Return the top-k users by frequency of event of event type
+#     """
 from dataclasses import dataclass
 import heapq
 from threading import Lock
@@ -135,8 +154,13 @@ class EventTracker:
         self.window_seconds = window_seconds
         self.bucket_size = bucket_size
 
+        # user related data structures
+        self.users: dict[str, set[str]] = {} # user id -> set of event ids
+
+        # event related data structures
         self.events: dict[str, Event] = {} # event id -> Event
         self.event_type_rbs: dict[str, RingBuffer] = {} # event type -> rb -> count
+        self.event_type_user_event_counts: dict[str, dict[str, int]] = {} # event type -> user id -> count
         self.event_types: set[str] = set()
 
         self.global_lock = Lock()
@@ -157,15 +181,23 @@ class EventTracker:
                 return
                 
             self.events[event_id] = event
-            if event_type not in self.event_type_rbs:
-                self.event_type_rbs[event_type] = RingBuffer(self.window_seconds, self.bucket_size)
-            rb = self.event_type_rbs[event_type]
+            rb = self.event_type_rbs.setdefault(event_type, RingBuffer(self.window_seconds, self.bucket_size))
             self.event_types.add(event_type)
+
+            user_events = self.users.setdefault(user_id, set())
+            new_user_event = event_id not in user_events
+            if new_user_event:
+                user_events.add(event_id)
+
+            _ = self.event_type_user_event_counts.setdefault(event_type, {}).setdefault(user_id, 0)
+            if new_user_event:
+                self.event_type_user_event_counts[event_type][user_id] += 1
+
             self.current_time = max(self.current_time, timestamp)
 
         rb.add(timestamp)
 
-    def update_event(self, event_id: str, user_id: str, new_timestamp: int) -> bool:
+    def update_event_timestamp(self, event_id: str, user_id: str, new_timestamp: int) -> bool:
         """
         Update the timestamp of a previously recorded event.
         Return False if the old event doesn’t exist or is too old to update.
@@ -180,6 +212,7 @@ class EventTracker:
             rb = self.event_type_rbs[event.event_type]
             old_timestamp = event.timestamp
             event.timestamp = new_timestamp
+            self.current_time = max(self.current_time, new_timestamp)
 
         if old_timestamp < now - self.window_seconds:
             return False
@@ -189,10 +222,13 @@ class EventTracker:
 
         return True
 
-    def delete_event(self, event_id, user_id: str) -> bool:
+    def delete_event(self, event_id: str) -> bool:
         """
         Delete a previously recorded event.
         Return False if the event doesn’t exist or is outside the current tracking window.
+        If event's timestamp lies outside the current tracking window
+        - Count in the ring buffer's will not be adjusted
+        - Event metadata will still be deleted
         """
         with self.global_lock:
             if event_id not in self.events:
@@ -204,12 +240,51 @@ class EventTracker:
             rb = self.event_type_rbs[event.event_type]
             del self.events[event_id]
 
+            user_id = event.user_id
+            self.users[event.user_id].discard(event_id)
+            if event.event_type in self.event_type_user_event_counts:
+                user_event_counts = self.event_type_user_event_counts[event.event_type]
+                if user_id in user_event_counts:
+                    user_event_counts[user_id] = max(0, user_event_counts[user_id]-1)
+
         if old_timestamp < now - self.window_seconds:
             return False
 
         rb.minus(old_timestamp)
         return True
-        
+
+    def delete_user(self, user_id: str) -> bool:
+        """
+        Delete an existing user and all user related data.
+        Return False if the user doesn’t exist.
+        """
+        with self.global_lock:
+            if user_id not in self.users:
+                return False
+                
+            now = self.current_time
+            cutoff = now - self.window_seconds
+            user_events = self.users.pop(user_id, set())
+            events = [self.events.pop(event_id, None) for event_id in user_events]
+
+        event_types: set[str] = set()
+        events = [event for event in events if event is not None]
+        for event in events:
+            rb = self.event_type_rbs.get(event.event_type)
+            if rb and event.timestamp >= cutoff:
+                rb.minus(event.timestamp, 1)
+            event_types.add(event.event_type)
+
+        with self.global_lock:
+            for event_type in event_types:
+                if event_type in self.event_type_user_event_counts:
+                    self.event_type_user_event_counts[event_type].pop(user_id, None)
+                if not self.event_type_user_event_counts[event_type]:
+                    self.event_type_user_event_counts.pop(event_type, None)
+                    self.event_type_rbs.pop(event_type, None)
+                    self.event_types.discard(event_type)
+        return True
+                    
     def get_event_count(self, event_type: str, last_t_seconds: int) -> int:
         """
         Return the approximate number of events of the given type within the last T seconds.
@@ -239,4 +314,32 @@ class EventTracker:
         )
 
         return [event_type for event_type, _ in top_k]
+
+    def get_global_top_active_users(self, k: int) -> list[str]:
+        """
+        Return the top-k users by frequency of events
+        """
+        with self.global_lock:
+            user_events = dict(self.users)
+
+        return heapq.nlargest(
+            k,
+            user_events.keys(),
+            key=lambda user_id: len(user_events[user_id]),
+        )
         
+
+    def get_global_top_active_users_by_event_type(self, event_type: str, k: int) -> list[str]:
+        """
+        Return the top-k users by frequency of event of event type
+        """
+        with self.global_lock:
+            if event_type not in self.event_type_user_event_counts:
+                return []
+            user_event_counts: dict[str, int] = dict(self.event_type_user_event_counts[event_type])
+
+        return heapq.nlargest(
+            k,
+            user_event_counts.keys(),
+            key=lambda user_id: user_event_counts[user_id],
+        )
